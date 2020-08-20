a8d30cda18db69fac65a957542e5a45447d9c2a0
Scarlet Widow <60269345+scarletwidown@users.noreply.github.com>
2020-08-20 13:12:15 -0400

    Finish App Component, Fix SearchBar error

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -29,7 +29,7 @@ class SearchBar extends React.Component {
                     <input placeholder="Where?"/>
                 </div>
                 <div className="SearchBar-submit">
-                    <a>Let's Go</a>
+                    <button>Let's Go</button>
                 </div>
             </div>
         );

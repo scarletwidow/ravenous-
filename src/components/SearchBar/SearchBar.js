942960b31ac1b48c0a815f001758303e6cab84dc
Scarlet Widow <60269345+scarletwidown@users.noreply.github.com>
2020-08-20 12:45:52 -0400

    Complete SearchBar Component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,8 +7,33 @@ const sortByOptions = {
     'Most Reviewed':  'review_count'
 }
 
+
 class SearchBar extends React.Component {
     renderSortByOptions() {
-        
+        return Object.keys(sortByOptions).map(sortByOption => {
+            const sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}>{sortByOption}</li>;
+        });
+    };
+
+    render() {
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-feields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?"/>
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+                </div>
+            </div>
+        );
     }
-}
+};
+
+export default SearchBar;

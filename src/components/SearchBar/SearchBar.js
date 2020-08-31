9ac3eb0c1e27e3f0fe6db15469a527a034e4095d
Scarlet Widow <60269345+scarletwidown@users.noreply.github.com>
2020-08-31 18:16:32 -0400

    Start Search Bar Functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed':  'review_count'
-}
+
 
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {term: '', location: '', sortBy: 'best_match'};
+        this.sortByOptions = {'Best Match': 'best_match', 'Highest Rated': 'rating', 'Most Reviewed':  'review_count'}
+    }
+
+    getSortByClass(sortByOption) {
+        if(this.state.sortBy === sortByOption){
+            return 'active';
+        } else {
+            return '';
+        }
+    }
+
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            const sortByOptionValue = sortByOptions[sortByOption];
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            const sortByOptionValue = this.sortByOptions[sortByOption];
             return <li key={sortByOptionValue}>{sortByOption}</li>;
         });
     };
